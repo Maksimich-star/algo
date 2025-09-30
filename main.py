cb3ca6d81cd8df14f336409c143227785f9a4358
Maksimich-star <mosyakin2009@mail.ru>
2025-09-30 18:57:10 +0300

    add decor and logs

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+from loguru import logger
+import time
+import random
+
+logger.add("log.log")
+
+def my_decor(func):
+    def wrapper(*args):
+        logger.info("мой декоратор начал работать")
+        logger.info(f"входные данные: {args}")
+        start = time.time()
+        result=func(*args)
+        end = time.time() - start
+        logger.info(f"выходные данные: {result}")
+        logger.success(f"декоратор закончил работу. функция отработала за {round(end,4)}")
+        return result
+    return wrapper
+
+
+@my_decor
 def bubble(data):
     N = len(data)
     for i in range(0, N - 1):
@@ -6,7 +26,7 @@ def bubble(data):
                 data[g], data[g + 1] = data[g + 1], data[g]
     return data
 
-
+@my_decor
 def merge_list(a, b):
     c = []
     N = len(a)
@@ -21,6 +41,7 @@ def merge_list(a, b):
             c.append(b[j])
             j += 1
     c += a[i:] + b[j:]
+    print(c)
     return c
 
 
@@ -34,10 +55,10 @@ def split_merge(data):
     return merge_list(a, b)
 
 
-mylist = [33, 61, 7, 2, 94, 562, -4, 0]
-# sort_mylist = bubble(mylist)
-# print(sort_mylist)
-sort_mylist1 = split_merge(mylist)
-print(sort_mylist1)
+# mylist = [33, 61, 7, 2, 94, 562, -4, 0]
+mylist = [random.randint(1, 1000) for i in range(1500) ]
+sort_mylist = bubble(mylist)
+# sort_mylist1 = split_merge(mylist)
+# print(sort_mylist1)
 
 
